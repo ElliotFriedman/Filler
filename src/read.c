eb953fd80cd9400372029dddefcfca5c27048d43
Elliot Friedman <efriedma@e1z2r3p16.42.us.org>
2018-08-12 15:57:09 -0700

    Makefile added

--- src/read.c
+++ src/read.c
@@ -2,7 +2,12 @@
 
 void		get_plateau(void)
 {
-	char	
-	
+	char	buf[2];
+
+	buf[0] = 0;
+	while (read(0, buf, 1) > 0)
+		printf("%s", buf);
+	printf("\n");
+
 
 }
--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-     #include <sys/types.h>
-     #include <sys/uio.h>
-     #include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 #include <string.h>
 
 //max width = 99
@@ -10,7 +10,6 @@
 int main()
 {
 	char	buf[2];
-	
 
 	buf[0] = 0;
 	while (read(0, buf, 1) > 0)
