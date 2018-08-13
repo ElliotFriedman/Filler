a7548dfa75374890480b868ee778003d86d12925
Elliot Friedman <elliotfriedman3@gmail.com>
2018-08-12 22:24:18 -0700

    building out main, created auxillary functions to grab map dimensions and player info

--- read.c
+++ read.c
@@ -1,20 +1,129 @@
 #include "filler.h"
 
+//x,y coords for how big the input block we have to place
+int		g_yin;
+int		g_xin;
+
+//x,y coords for how big the map is
+int		g_mapx;
+int		g_mapy;
+
+//number your player is
+int		g_num;
+//character that represents your player
+char	g_char;
+
+/*
+void	push_coord(char **str, int *x, int *y)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < g_mapx)
+	{
+		
+
+
+	}
+
+
+}
+
+void	create_cord(char *str, int x1, int y1)
+{
+	int		x[x1];
+	int		y[y1];
+	size_t	ix;
+	size_t	iy;
+	char	**split;
+
+	g_yin = y1;
+	g_xin = x1;
+	iy = 0;
+	ix = 0;
+	i = 0;
+	split = ft_strsplit(str, '\n');
+	ft_bzero((void*)x, x1 * 4);
+	ft_bzero((void*)y, y1 * 4);
+	while (split[ix] && split[ix][iy])
+	{
+		if (split[ix][iy] == '*')
+		{
+			x[i] = ix;
+			y[i] = iy;
+			i++;
+		}
+		iy++;
+		if (!split[ix][iy])
+		{
+			g_mapy = iy;
+			iy = 0;
+			ix++;
+		}
+	}
+	g_mapx = ix;
+	push_coord	
+}
+*/
+
+int		find_plateau(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (ft_strstr(str, "Plateau"))
+	{
+		while (str[i] && str[i] != ' ')
+			i++;
+		g_mapx = ft_atoi((const char *)&str[i]);
+		while (str[i] && str[i] == ' ')
+			i++;
+		while (str[i] && ft_isdigit(str[i]))
+			i++;
+		g_mapy = ft_atoi((const char *)&str[i]);
+		ft_putendl("Found plateau\n");
+		return (1);
+	}
+	return (0);
+}
+
+void	find_player(void)
+{
+	char	*buf;
+
+	if (!get_next_line(0, &buf))
+	{
+		ft_putendl("\nError, could not read in player number\n\n");
+		exit(0);
+	}
+	if (ft_strstr(buf, "p2"))
+	{
+		g_num = 2;
+		g_char = 'X';
+	}
+	else if (ft_strstr(buf, "p1"))
+	{
+		g_num = 1;
+		g_char = 'O';
+	}
+}
+
 int		main(void)
 {
 	char	*buf;
 	int		i = 1;
 
-	ft_dprintf(0, "hello world\n");
-
+	find_player();
 	while (i)
 	{
+		if (get_next_line(0, &buf))
+			find_plateau(buf);
 		while (get_next_line(0, &buf) > 0)
 		{
-			ft_dprintf(1, "\n%s\n", buf);
+			ft_dprintf(1, "%s\n", buf);
 			free(buf);
 		}
-		i = 0;
+	//	i = 0;
 	}
 	return 0;
 }
